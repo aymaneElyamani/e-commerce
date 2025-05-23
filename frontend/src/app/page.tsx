8ac6fbf6a803738c1a8da573644f29d82f47b7a1
Mohamed <mohamedaassouu@gmail.com>
2025-05-23 01:08:23 +0100

    feat: add Kids Collection component with images and customer URL functionality

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,46 +1,47 @@
+"use client";
 
-"use client"
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { getProfile, logoutUser } from "@/services/auth";
+import { getProfile } from "@/services/auth";
 import { WhatWeOffer } from "./_components/WhatWeOffer";
 import { FeaturedProductsSection } from "./_components/FeaturedProductsSection";
 import { TestimonialsSection } from "./_components/TestimonialsSection";
-// import { FooterSection } from "@/common/FooterSection";
 import useAuthStore from "@/store/useAuthStore";
 import { CategoriesCloches } from "./_components/categoriesClothes";
-import Hero from "./_components/Hero";
 import { BenefitsSection } from "./_components/BenefitsSection";
 import { HeroSection } from "./_components/HeroSection";
 import { CallToActionSection } from "./_components/CallToActionSection";
 
-
 const Home = () => {
-  // const dispatch = useDispatch();
   const router = useRouter();
-  const { user, isAuthenticated  , setProfile , logout} = useAuthStore();
-  useEffect(() => {
-     const token = localStorage.getItem('token');
+  const { setProfile } = useAuthStore();
 
+  const customerUrl = process.env.NEXT_PUBLIC_CUSTOMER_URL;
+ 
+  console.log("customerUrl", customerUrl);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
     if (token) {
-      getProfile({token : token ?? ""}).then(user => setProfile(user));
-    }else{
-      // router.push("/login")
+      getProfile({ token }).then((user) => setProfile(user));
     }
-  }, [isAuthenticated   ]);
+  }, []);
 
   return (
-    <div>
+    <div className="relative">
       <HeroSection />
-               {/* <Hero /> */}
-       <CategoriesCloches />
-       <WhatWeOffer />
-       <BenefitsSection />
-       {/* <FeaturedProductsSection /> */}
+      <CategoriesCloches />
+      <WhatWeOffer />
+      <BenefitsSection />
+      <TestimonialsSection />
 
-       <TestimonialsSection />
-
-    
+      {customerUrl && (
+        <button
+          onClick={() => router.push(customerUrl)}
+          className="fixed z-50 bottom-4 right-4 bg-green-600 hover:bg-green-700 text-white font-bold py-3 px-5 rounded-lg shadow-md z-50"
+        >
+          Create my Customer t-shirt
+        </button>
+      )}
     </div>
   );
 };
